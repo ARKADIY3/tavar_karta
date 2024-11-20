5c3c4cdc3e91a8e490d4b9744fb243608f0d0f27
arkadij3 <nagabedanarkadij2@gmail.com>
2024-11-20 23:27:43 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,55 @@
+const data =[{
+    model:'Sony',
+    price:'290',
+    color:'синей',
+    img: 'images/SONY.jpg',
+    reviews: 15
+},{
+    model:'NOKIA',
+    price:'2900',
+    color:'синей1',
+    img: 'images/NOKIA.jpg',
+    reviews: 150
+},{
+    model:'REDMI',
+    price:'29000',
+    color:'синей2',
+    img: 'images/REDMI.jpg',
+    reviews: 1500
+},{
+    model:'SAMSUNG',
+    price:'29000',
+    color:'синей2',
+    img: 'images/SAMSUNG.jpg',
+    reviews: 1500
+},{
+    model:'INFINEX',
+    price:'29000',
+    color:'синей2',
+    img: 'images/infinix.jpeg',
+    reviews: 1500 
+},{
+    model:'REALMI',
+    price:'29000$',
+    color:'синей2',
+    img: 'images/REALMI.jpg',
+    reviews: 1500
+},{
+    model:'REALMI1',
+    price:'29000$',
+    color:'синей2',
+    img: 'images/REALMI.jpg',
+    reviews: 1500
+},{
+    model:'REALMI2',
+    price:'29000$',
+    color:'синей2',
+    img: 'images/REALMI.jpg',
+    reviews: 1500
+},{
+    model:'REALMI3',
+    price:'29000$',
+    color:'синей2',
+    img: 'images/REALMI.jpg',
+    reviews: 1500
+}];
